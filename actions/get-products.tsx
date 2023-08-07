611fa9fbf3015598a32dcba8a8c767c06547d633
SCASTILLOH <sebastian.castillo.83z@gmail.com>
2023-08-07 15:06:11 -0400

    crea filter product

--- actions/get-products.tsx
+++ actions/get-products.tsx
@@ -7,21 +7,21 @@ interface Query {
   categoryId?: string;
   colorId?: string;
   sizeId?: string;
-  isFeatured: boolean;
+  isFeatured?: boolean;
 }
 
 const getProducts = async (query: Query): Promise<Product[]> => {
   const url = qs.stringifyUrl({
     url: URL,
     query: {
-      categoryId: query.categoryId,
       colorId: query.colorId,
       sizeId: query.sizeId,
+      categoryId: query.categoryId,
       isFeatured: query.isFeatured,
     },
   });
 
-  const res = await fetch(URL);
+  const res = await fetch(url);
 
   return res.json();
 };
---
+++ components/gallery/gallery-tab.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { cn } from "@/libs/utils";
+import { Image as ImageType } from "@/types";
+import React from "react";
+import { Tab } from "@headlessui/react";
+import Image from "next/image";
+
+interface GalleryTabProps {
+  image: ImageType;
+}
+
+const GalleryTab: React.FC<GalleryTabProps> = ({ image }) => {
+  return (
+    <Tab className="relative flex aspect-square cursor-pointer items-center justify-center bg-white rounded-md">
+      {({ selected }) => (
+        <div className="">
+          <span className="absolute h-full w-full aspect-square inset-0 overflow-hidden rounded-md">
+            <Image
+              src={image.url}
+              fill
+              alt="image"
+              className="object-cover object-center"
+            />
+          </span>
+          <span
+            className={cn(
+              "absolute inset-0 rounded-md ring-2 ring-offset-2",
+              selected ? "ring-black" : "ring-transparent"
+            )}
+          />
+        </div>
+      )}
+    </Tab>
+  );
+};
+
+export default GalleryTab;
