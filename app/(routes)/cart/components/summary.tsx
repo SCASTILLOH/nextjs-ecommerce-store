b567d7b2a4aa3d48e0192ba5253d2d114d0de8f0
SCASTILLOH <sebastian.castillo.83z@gmail.com>
2023-08-08 11:36:09 -0400

    implementa checkout con stripe

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import axios from "axios";
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+
 import Button from "@/components/ui/button";
 import Currency from "@/components/ui/currency";
 import useCart from "@/hooks/use-cart";
-import axios from "axios";
-import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
-import toast from "react-hot-toast/headless";
+import { toast } from "react-hot-toast";
 
 const Summary = () => {
   const searchParams = useSearchParams();
@@ -19,8 +20,8 @@ const Summary = () => {
       removeAll();
     }
 
-    if (searchParams.get("Caceled")) {
-      toast.error("Something went wrong");
+    if (searchParams.get("canceled")) {
+      toast.error("Something went wrong.");
     }
   }, [searchParams, removeAll]);
 
@@ -32,7 +33,7 @@ const Summary = () => {
     const response = await axios.post(
       `${process.env.NEXT_PUBLIC_API_URL}/checkout`,
       {
-        productsId: items.map((item) => item.id),
+        productIds: items.map((item) => item.id),
       }
     );
 
@@ -41,14 +42,18 @@ const Summary = () => {
 
   return (
     <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
-      <h2 className="text-lg font-medium text-gray-900">Order Summary</h2>
+      <h2 className="text-lg font-medium text-gray-900">Order summary</h2>
       <div className="mt-6 space-y-4">
         <div className="flex items-center justify-between border-t border-gray-200 pt-4">
-          <div className="text-base font-medium text-gray-900">Order Total</div>
+          <div className="text-base font-medium text-gray-900">Order total</div>
           <Currency value={totalPrice} />
         </div>
       </div>
-      <Button onClick={onCheckout} className="w-full mt-6">
+      <Button
+        onClick={onCheckout}
+        disabled={items.length === 0}
+        className="w-full mt-6"
+      >
         Checkout
       </Button>
     </div>
